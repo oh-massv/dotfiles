55665df891b9fbb2c6e96b54a18850ce95b6bef1
oh-massv <trk0364@gmail.com>
2022-10-03 05:28:04 +0900

    add starship config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,8 +15,13 @@ set -x PATH $HOME/.anyenv/bin $PATH
 # referred to https://hi120ki.github.io/blog/posts/20190125/
 anyenv init - fish | source
 
-set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --color=dark --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe --color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef'
+set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --info inline --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:-1,hl+:#d858fe --color=info:#98c379,prompt:#61afef,pointer:#98c379,marker:#e5c07b,spinner:#61afef,header:#61afef'
+#set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --inline-info --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe --color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef'
 
+set -U fish_user_paths (echo $fish_user_paths | tr ' ' '\n' | sort -u)
+
+starship init fish | source
+#eval "$(starship init fish)"
 # --- plugins ---
 #jorgebucaran/fisher
 #jethrokuan/z
