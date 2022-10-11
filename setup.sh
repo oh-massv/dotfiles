ae737be0a53d41afd2da04639c7f2d2689c971f4
oh-massv <trk0364@gmail.com>
2022-10-12 02:21:58 +0900

    add kitty config

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 echo "Installing dotfiles."
 
-ln -sf `pwd`/.zshrc ~/
-ln -sf `pwd`/.wezterm.lua ~/
-ln -sf `pwd`/.alacritty.yml ~/
-ln -sf `pwd`/.config/nvim ~/.config
-ln -sf `pwd`/.config/fish/config.fish ~/.config/fish
-ln -sf `pwd`/.yabairc ~/
-ln -sf `pwd`/.skhdrc ~/
+ln -sf $PWD/.zshrc ~/
+ln -sf $PWD/.wezterm.lua ~/
+ln -sf $PWD/.config/alacritty/alacritty.yml ~/.config/alacritty
+ln -sf $PWD/.config/kitty ~/.config
+ln -sf $PWD/.config/nvim ~/.config
+ln -sf $PWD/.config/zellij ~/.config
+ln -sf $PWD/.config/fish/config.fish ~/.config/fish
+ln -sf $PWD/.config/starship.toml ~/.config
+ln -sf $PWD/.yabairc ~/
+ln -sf $PWD/.skhdrc ~/
 
 # --- need ---
 # fish
