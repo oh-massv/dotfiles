d5d68a22a8f67bcbbc504f289ecc3853f835a4f6
oh-massv <trk0364@gmail.com>
2022-10-16 05:55:25 +0900

    format file

--- .config/nvim/lua/user/nvim-cmp.lua
+++ .config/nvim/lua/user/nvim-cmp.lua
@@ -1,28 +1,28 @@
 local status, cmp = pcall(require, "cmp")
 if (not status) then return end
 
-local has_words_before = function ()
+local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local lspkind = require 'lspkind'
-local luasnip = require("luasnip")
+local lspkind = require "lspkind"
+local luasnip = require "luasnip"
 
 cmp.setup({
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -32,9 +32,9 @@ cmp.setup({
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
 
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -42,21 +42,21 @@ cmp.setup({
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
+    { name = "nvim_lsp" },
+    { name = "luasnip" }, -- For luasnip users.
     -- { name = 'vsnip' }, -- For vsnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
   }, {
-    { name = 'buffer' },
+    { name = "buffer" },
   }),
   view = {
     entries = {
-      name = 'custom',
-      selection_order = 'near_cursor',
+      name = "custom",
+      selection_order = "near_cursor",
     },
   },
   window = {
@@ -64,39 +64,41 @@ cmp.setup({
     documentation = cmp.config.window.bordered(),
   },
   formatting = {
+    -- fields = { "kind", "abbr", "menu" },
     format = lspkind.cmp_format({
       -- with_text = false,
       -- maxwidth = 50
       menu = ({
-        buffer = "[Buffer]",
-        nvim_lsp = "[LSP]",
-        luasnip = "[LuaSnip]",
+        nvim_lsp = "λ",
+        luasnip = "⋗",
+        buffer = "Ω",
+        path = "[Path]",
         nvim_lua = "[Lua]",
         latex_symbols = "[Latex]",
       }),
     }),
   },
   experimental = {
-    ghost_text = true,
+    ghost_text = false,
   }
 })
 
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = 'path' },
-    { name = 'cmdline' }
+    { name = "path" },
+    { name = "cmdline" }
   }
 })
 
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline("/", {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = 'buffer' }
+    { name = "buffer" }
   },
   view = {
     entries = {
-      name = 'wildmenu', separator = '|'
+      name = "wildmenu", separator = "|"
     }
   },
 })
