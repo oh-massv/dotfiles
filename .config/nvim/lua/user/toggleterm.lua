3d451498dbb1ada330f81a1379e7452002c1a28b
oh-massv <trk0364@gmail.com>
2022-10-16 05:54:16 +0900

    update toggleterm config

--- .config/nvim/lua/user/toggleterm.lua
+++ .config/nvim/lua/user/toggleterm.lua
@@ -1,17 +1,45 @@
+local M = {}
+
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then return end
 
 toggleterm.setup({
-  open_mapping = [[<c-\>]]
+  active = true,
+  open_mapping = [[<c-\>]],
+  float_opts = {
+    border = "curved",
+  },
 })
+
+-- local Terminal = require("toggleterm.terminal").Terminal
+-- local lazygit = Terminal:new({
+--   cmd = "lazygit",
+--   direction = "float",
+--   hidden = true
+-- })
+
+-- function _lazygit_toggle()
+--   lazygit:toggle()
+-- end
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({
-  cmd = "lazygit",
-  direction = "float",
-  hidden = true
-})
 
-function _lazygit_toggle()
-  lazygit:toggle()
+M.toggle_float = function()
+  local float = Terminal:new({ direction = "float" })
+  float:toggle()
+  -- return float:toggle()
 end
+
+M.toggle_lazygit = function()
+  local lazygit = Terminal:new({
+    cmd = "lazygit",
+    direction = "float",
+    float_opts = {
+      -- border = "none",
+    }
+  })
+  lazygit:toggle()
+  -- return lazygit:toggle()
+end
+
+return M
