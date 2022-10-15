01d29dd6cb4037deada19ccd501e4895e0110f89
oh-massv <trk0364@gmail.com>
2022-10-16 05:50:42 +0900

    update nvim options

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,6 +1,6 @@
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 
 vim.opt.number = true
 
@@ -14,23 +14,23 @@ vim.opt.cmdheight = 1
 vim.opt.laststatus = 3
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
-vim.opt.shell = 'fish'
-vim.opt.backupskip = { '/tmp/*', '/private/tmp/*' }
-vim.opt.inccommand = 'split'
+vim.opt.shell = "fish"
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.inccommand = "split"
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.wrap = false -- No Wrap lines
-vim.opt.backspace = { 'start', 'eol', 'indent' }
-vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
-vim.opt.wildignore:append { '*/node_modules/*' }
-vim.opt.signcolumn = 'yes'
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append { "**" } -- Finding files - Search down into subfolders
+vim.opt.wildignore:append { "*/node_modules/*" }
+vim.opt.signcolumn = "yes"
 
 vim.opt.timeoutlen = 300
 
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 vim.opt.hidden = true
 vim.opt.lazyredraw = true
@@ -38,18 +38,21 @@ vim.opt.autoread = true
 vim.opt.showmode = false
 vim.opt.swapfile = false
 vim.opt.clipboard = "unnamedplus"
-vim.opt.splitbelow = true   -- force all horizontal splits to go below current window
-vim.opt.splitright = true   -- force all vertical splits to go to the right of current window
-vim.opt.undofile = true     -- enable persistent undo
-vim.opt.updatetime = 100    -- faster completion (4000ms default)
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.updatetime = 100 -- faster completion (4000ms default)
+
 vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = "pum"
+vim.opt.pumblend = 5
+vim.opt.background = "dark"
 
 -- Undercurl
 -- vim.cmd([[let &t_Cs = "\e[4:3m"]])
 -- vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Add asterisks in block comments
-vim.opt.formatoptions:append { 'r' }
-
--- winbar
--- vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+vim.opt.formatoptions:append { "r" }
