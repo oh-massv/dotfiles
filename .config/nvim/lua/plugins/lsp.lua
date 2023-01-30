0a22d6a816a91e49c0145bbb6fa0ace7428f81f5
oh-massv <trk0364@gmail.com>
2023-01-30 09:32:47 +0900

    add null-ls & mason config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,1 +1,40 @@
-return {}
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.prettier.with {
+            prefer_local = "node_modules/.bin",
+            -- extra_filetypes = { "toml", "solidity" },
+            -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+          },
+          nls.builtins.formatting.textlint.with({
+            filetypes = { "markdown" },
+          }),
+          nls.builtins.formatting.markdownlint,
+          nls.builtins.formatting.stylelint,
+          nls.builtins.diagnostics.textlint.with({
+            filetypes = { "markdown" },
+          }),
+          nls.builtins.diagnostics.markdownlint,
+          nls.builtins.diagnostics.stylelint,
+        },
+      }
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "css-lsp",
+        "cssmodules-language-server",
+        "typescript-language-server",
+        "eslint-lsp",
+        -- "textlint",
+        -- "markdownlint",
+      },
+    },
+  },
+}
