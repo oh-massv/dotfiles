28b0a472b6483fea8cb6e10938e58d5ab9d2a4c0
oh-massv <trk0364@gmail.com>
2023-11-22 06:22:36 +0900

    fish: clean up & add theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,34 +1,36 @@
-#set PATH /opt/homebrew/bin $PATH
-#set PATH $HOME/Developer/flutter/bin $PATH
-#set PATH /opt/homebrew/opt/python@3.9/libexec/bin $PATH
+# general bin paths
+fish_add_path /usr/local/bin
+fish_add_path $HOME/bin # will be moved
+
+# go
 set -gx GOPATH (go env GOPATH)
-#set -x PATH $PATH (go env GOPATH)/bin
 fish_add_path (go env GOPATH)/bin
 
-abbr vi 'nvim'
+# rust
+fish_add_path $HOME/.cargo/bin
 
-# ref: https://hi120ki.github.io/blog/posts/20190125/
-# anyenv init - fish | source
+# asdf
+set -gx ASDF_CONFIG_FILE $HOME/.config/asdf/.asdfrc
+source /opt/homebrew/opt/asdf/libexec/asdf.fish # Added by asdf
 
-set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --info inline --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:-1,hl+:#d858fe --color=info:#98c379,prompt:#61afef,pointer:#98c379,marker:#e5c07b,spinner:#61afef,header:#61afef'
-
-set -U fish_user_paths (echo $fish_user_paths | tr ' ' '\n' | sort -u)
-
-set -gx XDG_CONFIG_HOME $HOME/.config
-
-set -gx EDITOR nvim
-
-starship init fish | source
-
-set -x ASDF_CONFIG_FILE $HOME/.config/asdf/.asdfrc
-
-# --- plugins ---
-#jorgebucaran/fisher
-#jethrokuan/z
-#jethrokuan/fzf
-#decors/fish-ghq
-#rafaelrinaldi/pure
-
+# docker
 source /Users/massv/.docker/init-fish.sh || true # Added by Docker Desktop
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish # Added by asdf
+# commands
+abbr -a cp 'cp -i' # or alias cp='cp -i'
+abbr -a mv 'mv -i'
+abbr -a rm 'trash-put'
+abbr -a vi 'nvim'
+abbr -a . cd ../
+abbr -a .. cd ../../
+abbr -a ... cd ../../../
+
+# global variables
+set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --info inline --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:-1,hl+:#d858fe --color=info:#98c379,prompt:#61afef,pointer:#98c379,marker:#e5c07b,spinner:#61afef,header:#61afef'
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx EDITOR nvim
+set -gx TERM xterm-256color
+
+# set -U fish_user_paths (echo $fish_user_paths | tr ' ' '\n' | sort -u)
+
+starship init fish | source
