3df9b7863a94e50078bf05dfb1b43cbe72fbfd9c
oh-massv <7891377+oh-massv@users.noreply.github.com>
2025-08-25 16:21:46 +0900

    remove unused code

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -11,24 +11,6 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    -- opts = function(_, opts)
-    --   table.insert(opts.routes, {
-    --     filter = {
-    --       event = "notify",
-    --       find = "No information available",
-    --     },
-    --     opts = { skip = true },
-    --   })
-    --   opts.presets = {
-    --     -- command_palette = false,
-    --     lsp_doc_border = true,
-    --   }
-    --   opts.views = {
-    --     mini = {
-    --       win_options = { winblend = 0 },
-    --     },
-    --   }
-    -- end,
     opts = {
       views = {
         mini = {
@@ -64,71 +46,6 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     opts = {},
-  },
-  {
-    "echasnovski/mini.icons",
-    -- enabled = false,
-  },
-  {
-    {
-      "echasnovski/mini.statusline",
-      enabled = false,
-      version = "*",
-    },
-  },
-  {
-    "freddiehaddad/feline.nvim",
-    enabled = false,
-    config = function(_, opts)
-      local ctp_feline = require("catppuccin.groups.integrations.feline")
-
-      ctp_feline.setup({
-        view = {
-          lsp = {
-            progress = true, -- if true the status bar will display an lsp progress indicator
-            name = true, -- if true the status bar will display the lsp servers name, otherwise it will display the text "Lsp"
-            exclude_lsp_names = {}, -- lsp server names that should not be displayed when name is set to true
-            separator = "|", -- the separator used when there are multiple lsp servers
-          },
-        },
-      })
-
-      require("feline").setup({
-        components = ctp_feline.get(),
-        force_inactive = {
-          filetypes = {
-            "^NvimTree$",
-            "^packer$",
-            "^startify$",
-            "^fugitive$",
-            "^fugitiveblame$",
-            "^qf$",
-            -- "^help$",
-          },
-          buftypes = {},
-        },
-        disable = {
-          filetypes = {
-            "^Trouble$",
-            "^Outline$",
-            "^qf$",
-            "^git$",
-            "^dapui_scopes$",
-            "^dapui_breakpoints$",
-            "^dapui_stacks$",
-            "^dapui_watches$",
-            "^dapui_repl$",
-            "^dap%-repl$",
-            "^dapui_console$",
-            "^Codewindow$",
-            "^JABSwindow$",
-            "^dashboard$",
-            "^alpha$",
-            "^starter$",
-          },
-        },
-      })
-    end,
   },
   {
     "folke/snacks.nvim",
